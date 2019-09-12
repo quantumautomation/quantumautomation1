7e0710ce78f620ba6fa40053bce0e5a8cff6e6a2
Kevin <kevinmpqa@gmail.com>
2019-09-12 11:01:11 -0700

    Test Git commit

---
+++ CompareStrings.js
@@ -0,0 +1,14 @@
+//Compare 2 strings
+
+var mystr1 = "Compare 2 strings" 
+var mystr2 = "Compare strings"
+
+if(mystr1==mystr2)
+{
+	console.log("the strings are the same")
+}
+
+else
+{
+	console.log("the strings are not the same")
+}
